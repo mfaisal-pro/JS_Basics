05beae557091650370f092d4fb1291d6bd177ae9
invigilator05 <invigilator05@LRO-SH-04.vu.edu.pk>
2024-11-18 16:52:54 +0500

    some code writes here

---
+++ first.js
@@ -0,0 +1,138 @@
+
+// fullname = "Muhammad Faisal";
+// console.log(fullname);
+// age = 24
+// price = 99.99
+// radius = 14
+// x = null
+// y = undefined
+// isFollow = false;
+
+// fullName = 25;
+// console.log(fullName)
+// console.log("hello world");
+
+// Variables
+
+// "var" keyword was used in old javascrip versions 
+// it is not a good practice to declare variables using var keyword in modern javascript
+
+// var n=10;
+// console.log(n)
+// console.log('n')
+
+// var m= "hello"
+// console.log(m)
+
+// n=50
+// console.log(n)
+
+// var n="hello"
+// console.log(n)
+
+// mostly we will use "let" and "const" as variable names 
+
+// we can not re-define a variable using "let" keyword, one it is declared 
+// we cannot use let n again to declare a variable 
+
+    // let n=10;
+    // console.log(n)
+
+    // let n=50
+
+// var varible can be called in and outside of the function avriable scope 
+
+// let variable cannot be called out side of the function variable scope 
+
+// {
+//     var a=56
+//     let n=23
+// }
+
+
+// It will not give an error beacuse it can be called in(private-scope) 
+// and outside(public-scope) of its declared scope 
+// console.log(a)
+
+// It will give an error, beacuse it cannot work outside(public-scope) of its declared scope 
+// console.log(n)
+
+// a variable cannot be declared using two words seprated by a space 
+
+// an underscore can be used instead of a space 
+
+// For Example 
+
+// Incorrect method
+
+// let abc hello=10   
+// console.log(abc hello)
+// It will give an error 
+
+// correct method 
+
+// let abc_hello=10    
+// console.log(abc_hello)
+// It will not give an error 
+
+// declaring multiple variable at one 
+
+// let c=5,b=6,d="hello";
+
+// console.log(d);
+
+
+// const f=5
+// console.log(f)
+
+// const f=6
+
+// const variable cannot be re-assigned
+
+// Strings and template literals 
+
+// a varible can be called inside a string
+
+// let n=10
+// applicable for a string in a comma above tab button of keyboard called backtick
+
+// let w=`Welcome ${n} Faisal ${10+20}`;
+// console.log(w)
+
+// a varible can be called inside a string and the result will be converted into string
+
+// There are two types of data types in Javascript
+// 1. Premitive
+// 2. Non-Premitive
+
+// Primitive data types are stored directly in the memory and are immutable
+
+// Primitive data type examples
+// 1.Number
+// 2.String
+// 3.Boolean
+// 4.null 
+// 5.undefined
+// 6.Symbol
+// 7.bigint
+
+// Non-Primitive data types are stored in the memory and are mutable
+
+// Examples
+
+// 1. Object
+// 2. Array
+
+
+// Datatype of a variable can be found using "typeof"
+
+// let num=20;
+// console.log(num)
+// console.log(typeof num);
+
+// // let num2='20'
+// // console.log(typeof num2)
+// let num2=20.5
+
+// console.log(num2, typeof num2)
+
